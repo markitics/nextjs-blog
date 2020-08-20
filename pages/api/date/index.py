37143c9cd3142366370b0ad896eac4a48c88da55
markitics <m@rkmoriarty.com>
2020-08-20 16:59:20 +0100

    Add python api endpoint to test python serverless functions

---
+++ pages/api/date/index.py
@@ -0,0 +1,13 @@
+from http.server import BaseHTTPRequestHandler
+from datetime import datetime
+
+class handler(BaseHTTPRequestHandler):
+
+  def do_GET(self):
+    self.send_response(200)
+    self.send_header('Content-type', 'text/plain')
+    self.end_headers()
+    mystring = "The time is: "
+    mystring += str(datetime.now().strftime('%Y-%m-%d %H:%M:%S')).encode()
+    self.wfile.write()
+    return
